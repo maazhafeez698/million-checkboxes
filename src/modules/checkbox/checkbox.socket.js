ca3a8144ec158c46855ac337106c6b84fbbf815e
Maaz Hafeez <maazhafeez698@gmail.com>
2026-08-22 18:13:31 +0500

    feat: add socket rate limiting

--- src/modules/checkbox/checkbox.socket.js
+++ src/modules/checkbox/checkbox.socket.js
@@ -1,21 +1,41 @@
 import { toggle } from "./checkbox.service.js";
 
+const RATE_LIMIT_MS = 500;
+
+const lastAction = new Map();
+
 export const registerCheckboxSocket = (io) => {
   io.on("connection", (socket) => {
     socket.on("checkbox:toggle", async (index, callback) => {
-      try {
-        const event = await toggle(Number(index));
+      const now = Date.now();
+      const last = lastAction.get(socket.id) || 0;
 
-        callback?.({
-          success: true,
-          ...event,
-        });
-      } catch (error) {
-        callback?.({
+      if (now - last < RATE_LIMIT_MS) {
+        return callback({
           success: false,
-          message: error.message,
+          message: "Please wait before clicking again.",
         });
       }
+
+      lastAction.set(socket.id, now);
+
+      try {
+        const result = await toggle(Number(index));
+
+        callback({
+          success: true,
+          ...result,
+        });
+      } catch (error) {
+        callback({
+          success: false,
+          message: error.message || "Unable to update checkbox.",
+        });
+      }
+    });
+
+    socket.on("disconnect", () => {
+      lastAction.delete(socket.id);
     });
   });
 };
